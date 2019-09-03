3dcc2b5208c08ee69ba322779b63b9b08b553d1c
Kim Bjørn Tiedemann <kbt@schultz.dk>
2019-09-03 09:04:38 +0200

    Changes

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php
-$welcomeText='<p>Hello MMA</p>';
-?>
-<?=$welcomeText;?>
-<?php
-$xmlstr = <<<XML
-<?xml version='1.0' standalone='yes'?>
-<questions>
- <question text='Do you like beer?'>
-  <answer text='YES!' />
- </question>
-</questions>
-XML;
+ $q = $_GET['q'];
 
-$questions = new SimpleXMLElement($xmlstr);
+ echo 'FUCK YOU MMA!!!! PHP STILL RULEZ THE WORLD';
+
 ?>
-<p>Spørgsmål : <?=$questions->question[0]['text']?></p>
-<p>Svar : <?=$questions->question[0]->answer['text']?>
